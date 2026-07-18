03b1cc1bae4750d0b6400289cbf28cf93ecdbc78
qzh <3023361831@qq.com>
2026-07-18 18:08:11 +0800

    chore(config): configure local dev port and disable Docker Compose startup

--- src/main/java/org/example/vocalchat/VocalChatApplication.java
+++ src/main/java/org/example/vocalchat/VocalChatApplication.java
@@ -1,9 +1,11 @@
 package org.example.vocalchat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.example.vocalchat.mapper")
 public class VocalChatApplication {
 
     public static void main(String[] args) {
