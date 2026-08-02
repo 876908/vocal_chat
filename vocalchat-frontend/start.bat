8d24863c384fd696da62444d81003fd21c4679c0
qzh <3023361831@qq.com>
2026-08-02 16:05:06 +0800

    fix:miniodelete

--- vocalchat-frontend/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-nginx -p . -c nginx.conf
-echo VocalChat Frontend: http://localhost
-pause
--- vocalchat-frontend/stop.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-nginx -p . -c nginx.conf -s stop
-echo Stopped.
-pause
